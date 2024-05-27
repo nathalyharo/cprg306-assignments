f559b0169fcb2828f176a0ecbf83f0112e4e5bbf
nathalyharo <nathaly.haro98@gmail.com>
2024-05-27 01:57:37 -0600

    assignment 2 requirements

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1></h1>
+      <Link href="https://github.com/nathalyharo" target="_blank">My GitHub Profile</Link>
+    </div>
+  );
+};
